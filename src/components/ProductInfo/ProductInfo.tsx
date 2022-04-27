bc4f10c0eab14d88eb5730aa9b0de128d4713d07
Polina_Svyazhina <kiramik55@gmail.com>
2022-04-27 16:03:48 +0500

    Temp.

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -50,9 +50,13 @@ export const ProductInfo: React.FC<ProductEditorProps> = ({ stateProduct, remove
           <p className={`titleSmall ${classes.title}`}>Замены</p>
           {stateProduct.replacement ? <p>{stateProduct.replacement}</p> : <p>—</p>}
         </div>
-        <Button onClick={() => SetIsEdit(true)}>Изменить</Button>
-        <Button className={clsx(classes.redButton, classes.button)} onClick={removeProduct}>
+      </div>
+      <div className={classes.buttons}>
+        <Button className={clsx(classes.redButton, classes.changeButton)} onClick={removeProduct}>
           Удалить
+        </Button>
+        <Button className={classes.deleteButton} onClick={() => SetIsEdit(true)}>
+          Изменить
         </Button>
       </div>
     </form>
